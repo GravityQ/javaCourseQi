bf5cd83f292c040f8a211b5fbb8f28056eb1f227
qijunlin <qijunlin@huobi.com>
2021-09-18 09:31:55 +0800

    项目初始化

---
+++ src/main/java/com/example/javacourseqi/JavaCourseQiApplication.java
@@ -0,0 +1,13 @@
+package com.example.javacourseqi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaCourseQiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavaCourseQiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/javacourseqi/JavaCourseQiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.javacourseqi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaCourseQiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
